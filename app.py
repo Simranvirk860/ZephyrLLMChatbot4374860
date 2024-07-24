d01acc33930921b015e1680e2081af3e6b037a47
Simranvirk860 <Si4374860@alphacollege.me>
2024-07-24 17:24:17 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def respond(
     temperature,
     top_p,
 ):
-    system_message = "You are a good listener. You advise relaxation exercises, suggest avoiding negative thoughts, and guide through steps to manage stress. Discuss what's on your mind, or ask me for a quick relaxation exercise."
+    system_message = "You are here to assist them with class schedules, instructor info, registration, membership plans, studio guidelines, yoga tips, and any other questions. Let's make their yoga journey smooth and enjoyable. "
     messages = [{"role": "system", "content": system_message}]
 
     for val in history:
@@ -46,7 +46,7 @@ For information on how to customize the ChatInterface, peruse the gradio docs: h
 demo = gr.ChatInterface(
     respond,
     additional_inputs=[
-        gr.Textbox(value = "You are a good listener. You advise relaxation exercises, suggest avoiding negative thoughts, and guide through steps to manage stress. Discuss what's on your mind, or ask me for a quick relaxation exercise.", label="System message"),
+        gr.Textbox(value = "You are here to assist them with class schedules, instructor info, registration, membership plans, studio guidelines, yoga tips, and any other questions. Let's make their yoga journey smooth and enjoyable.", label="System message"),
         gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
         gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
         gr.Slider(
@@ -59,11 +59,11 @@ demo = gr.ChatInterface(
     ],
 
     examples = [ 
-        ["I feel overwhelmed with work."],
-        ["Can you guide me through a quick meditation?"],
-        ["How do I stop worrying about things I can't control?"]
+        ["Can you provide tips for improving flexibility and balance?"],
+        ["What should I focus on to improve my posture and alignment?"],
+        ["How can I incorporate yoga into my daily routine?"],
     ],
-    title = 'Calm Mate 🕊️'
+    title = 'Yoga Instructor'
 )
 
 
